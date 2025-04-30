96df4629aa6348681a180491a13285c2db6214eb
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-30 20:04:21 +0200

    feat: initial setup of products page

---
+++ app/pages/products/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { data } = await useAsyncData('products', () => GqlGetProducts());
+
+const products = computed(() => data?.value?.products);
+
+if (!products.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product Not Found' });
+}
+</script>
+
+<template>
+  <div class="max-w-7xl px-6 text-center mx-auto">
+    {{ products }}
+  </div>
+</template>
