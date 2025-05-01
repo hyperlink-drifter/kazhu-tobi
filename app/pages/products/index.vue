0f1428c078f91e18485f7f54e6622c37d1a2a238
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-01 13:49:51 +0200

    feat: add links to products

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
 const { data } = await useAsyncData('products', () => GqlGetProducts());
 
-const products = computed(() => data?.value?.products);
+const products = computed(() => data.value?.products);
 
 if (!products.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product Not Found' });
+}
+
+if (!products.value.items) {
   throw createError({ statusCode: 404, statusMessage: 'Product Not Found' });
 }
 </script>
 
 <template>
   <div class="max-w-7xl px-6 text-center mx-auto">
-    {{ products }}
+    <ul>
+      <li v-for="product in products?.items" :key="JSON.stringify(product)">
+        <NuxtLink
+          :to="
+            $localePath({
+              name: 'products-slug',
+              params: { slug: product.slug },
+            })
+          "
+        >
+          {{ product.name }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
