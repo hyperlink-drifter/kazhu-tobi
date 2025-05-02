8420798dd0aac53e223e0ebf6cce260ab59a62a6
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-02 20:21:48 +0200

    refactor: improve hover behaviour of second image

--- app/components/product/TileCard.vue
+++ app/components/product/TileCard.vue
@@ -1,23 +1,6 @@
 <script lang="ts" setup>
 const props = defineProps<{ product: VendureProduct }>();
-
 const computedPrice = computed(() => {});
-
-const shouldDisplaySecondImage = ref(false);
-
-function handleMouseOver(event: MouseEvent) {
-  // On mobile devices, click event also triggers mouseover event
-  const isMobileClickEvent = !event.relatedTarget;
-
-  if (
-    isMobileClickEvent ||
-    (props.product?.assets && props.product?.assets?.length < 2)
-  ) {
-    return;
-  }
-
-  shouldDisplaySecondImage.value = true;
-}
 </script>
 
 <template>
@@ -29,25 +12,21 @@ function handleMouseOver(event: MouseEvent) {
       })
     "
     class="block h-full"
-    @click="shouldDisplaySecondImage = false"
-    @mouseover="handleMouseOver"
-    @mouseleave="shouldDisplaySecondImage = false"
   >
     <Card class="h-full py-0 rounded-none shadow-none border-none gap-4">
-      <CardHeader class="px-0">
+      <AspectRatio :ratio="4 / 5" class="group">
         <NuxtImg
-          v-show="!shouldDisplaySecondImage"
           :src="product?.featuredAsset?.source"
-          :alt="`Image 1 of a product ${product?.name}`"
           :preload="true"
           loading="eager"
         />
         <NuxtImg
-          v-show="shouldDisplaySecondImage"
+          class="opacity-0 group-hover:pointer-fine:opacity-100 absolute top-0"
           :src="product?.assets?.[1]?.source"
-          :alt="`Image 2 of a product ${product?.name}`"
           loading="lazy"
         />
+      </AspectRatio>
+      <CardHeader class="px-0">
         <CardTitle>{{ product?.name }}</CardTitle>
       </CardHeader>
       <CardContent class="px-0">
