a0dcefce74b3f1cb6cbf97b236f692aaaa8ce540
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-03 11:57:40 +0200

    feat: initial setup for navigation menu

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { HTMLAttributes } from 'vue';
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 
 interface Props {
   class?: HTMLAttributes['class'];
@@ -10,6 +11,31 @@ const props = defineProps<Props>();
 
 <template>
   <header :class="cn(props.class)">
-    <slot> </slot>
+    <slot>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NuxtLink
+              custom
+              v-slot="{ isActive, href, navigate }"
+              :to="
+                $localePath({
+                  name: 'products',
+                })
+              "
+            >
+              <NavigationMenuLink
+                :active="isActive"
+                :href
+                :class="navigationMenuTriggerStyle()"
+                @click="navigate"
+              >
+                {{ $t('products') }}
+              </NavigationMenuLink>
+            </NuxtLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </slot>
   </header>
 </template>
