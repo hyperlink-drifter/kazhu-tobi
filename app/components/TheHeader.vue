ee3bfe8867fa7b55e967dd490ffe41e93d962bee
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-03 14:57:18 +0200

    refactor: move navigation in its own component

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,79 +1,19 @@
 <script lang="ts" setup>
 import type { HTMLAttributes } from 'vue';
-import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 
 interface Props {
   class?: HTMLAttributes['class'];
 }
 
 const props = defineProps<Props>();
-
-const { locale } = useI18n();
-
-const { data } = await useAsyncData(`collections`, () =>
-  GqlGetTopLevelCollections()
-);
-
-const collections = computed(() => data.value?.collections.items);
-
-const localeCollections = computed(() => {
-  return collections.value?.map((c) =>
-    c.translations.find((t) => t.languageCode === locale.value)
-  );
-});
 </script>
 
 <template>
-  <header :class="cn(props.class)">
-    <slot>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NuxtLink
-              custom
-              v-slot="{ isActive, href, navigate }"
-              :to="
-                $localePath({
-                  name: 'products',
-                })
-              "
-            >
-              <NavigationMenuLink
-                :active="isActive"
-                :href
-                :class="navigationMenuTriggerStyle()"
-                @click="navigate"
-              >
-                {{ $t('products') }}
-              </NavigationMenuLink>
-            </NuxtLink>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem v-for="collection in localeCollections">
-            <NuxtLink
-              custom
-              v-slot="{ isActive, href, navigate }"
-              :to="
-                $localePath({
-                  name: 'collections-slug',
-                  params: {
-                    slug: collection?.slug,
-                  },
-                })
-              "
-            >
-              <NavigationMenuLink
-                :active="isActive"
-                :href
-                :class="navigationMenuTriggerStyle()"
-                @click="navigate"
-              >
-                {{ collection?.name }}
-              </NavigationMenuLink>
-            </NuxtLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </slot>
-  </header>
+  <LayoutCenter>
+    <header :class="cn('flex justify-between', props.class)">
+      <slot>
+        <TheNavigation />
+      </slot>
+    </header>
+  </LayoutCenter>
 </template>
---
+++ app/components/TheNavigation.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue';
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+
+interface Props {
+  class?: HTMLAttributes['class'];
+}
+
+const props = defineProps<Props>();
+
+const { locale } = useI18n();
+
+const { data } = await useAsyncData(`collections`, () =>
+  GqlGetTopLevelCollections()
+);
+
+const collections = computed(() => data.value?.collections.items);
+
+const localeCollections = computed(() => {
+  return collections.value?.map((c) =>
+    c.translations.find((t) => t.languageCode === locale.value)
+  );
+});
+</script>
+
+<template>
+  <main :class="cn(props.class)">
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NuxtLink
+            custom
+            v-slot="{ isActive, href, navigate }"
+            :to="
+              $localePath({
+                name: 'products',
+              })
+            "
+          >
+            <NavigationMenuLink
+              :active="isActive"
+              :href
+              :class="navigationMenuTriggerStyle()"
+              @click="navigate"
+            >
+              {{ $t('products') }}
+            </NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem v-for="collection in localeCollections">
+          <NuxtLink
+            custom
+            v-slot="{ isActive, href, navigate }"
+            :to="
+              $localePath({
+                name: 'collections-slug',
+                params: {
+                  slug: collection?.slug,
+                },
+              })
+            "
+          >
+            <NavigationMenuLink
+              :active="isActive"
+              :href
+              :class="navigationMenuTriggerStyle()"
+              @click="navigate"
+            >
+              {{ collection?.name }}
+            </NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </main>
+</template>
