20674aa07457aa607719d0e95a0f990d47a9efbb
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-03 14:11:27 +0200

    feat: implement page navigation with internationalization of collections

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -7,6 +7,20 @@ interface Props {
 }
 
 const props = defineProps<Props>();
+
+const { locale } = useI18n();
+
+const { data } = await useAsyncData(`collections`, () =>
+  GqlGetTopLevelCollections()
+);
+
+const collections = computed(() => data.value?.collections.items);
+
+const localeCollections = computed(() => {
+  return collections.value?.map((c) =>
+    c.translations.find((t) => t.languageCode === locale.value)
+  );
+});
 </script>
 
 <template>
@@ -31,6 +45,30 @@ const props = defineProps<Props>();
                 @click="navigate"
               >
                 {{ $t('products') }}
+              </NavigationMenuLink>
+            </NuxtLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem v-for="collection in localeCollections">
+            <NuxtLink
+              custom
+              v-slot="{ isActive, href, navigate }"
+              :to="
+                $localePath({
+                  name: 'collections-slug',
+                  params: {
+                    slug: collection?.slug,
+                  },
+                })
+              "
+            >
+              <NavigationMenuLink
+                :active="isActive"
+                :href
+                :class="navigationMenuTriggerStyle()"
+                @click="navigate"
+              >
+                {{ collection?.name }}
               </NavigationMenuLink>
             </NuxtLink>
           </NavigationMenuItem>
--- modules/vendure/types/index.ts
+++ modules/vendure/types/index.ts
@@ -1,5 +1,12 @@
-import type { GetProductQuery, GetProductsQuery } from '#gql';
+import type {
+  GetProductQuery,
+  GetProductsQuery,
+  GetTopLevelCollectionsQuery,
+} from '#gql';
 
 // Product
 export type VendureProduct = GetProductQuery['product'];
 export type VendureProducts = GetProductsQuery['products'];
+
+// Collection
+export type VendureCollections = GetTopLevelCollectionsQuery['collections'];
