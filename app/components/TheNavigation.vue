d9f576f285b1d12d984d9e10cfee62541b3d7d9e
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-03 15:03:14 +0200

    fixup! refactor: move navigation in its own component

--- app/components/TheNavigation.vue
+++ app/components/TheNavigation.vue
@@ -24,54 +24,52 @@ const localeCollections = computed(() => {
 </script>
 
 <template>
-  <main :class="cn(props.class)">
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NuxtLink
-            custom
-            v-slot="{ isActive, href, navigate }"
-            :to="
-              $localePath({
-                name: 'products',
-              })
-            "
+  <NavigationMenu :class="cn(props.class)">
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NuxtLink
+          custom
+          v-slot="{ isActive, href, navigate }"
+          :to="
+            $localePath({
+              name: 'products',
+            })
+          "
+        >
+          <NavigationMenuLink
+            :active="isActive"
+            :href
+            :class="navigationMenuTriggerStyle()"
+            @click="navigate"
           >
-            <NavigationMenuLink
-              :active="isActive"
-              :href
-              :class="navigationMenuTriggerStyle()"
-              @click="navigate"
-            >
-              {{ $t('products') }}
-            </NavigationMenuLink>
-          </NuxtLink>
-        </NavigationMenuItem>
+            {{ $t('products') }}
+          </NavigationMenuLink>
+        </NuxtLink>
+      </NavigationMenuItem>
 
-        <NavigationMenuItem v-for="collection in localeCollections">
-          <NuxtLink
-            custom
-            v-slot="{ isActive, href, navigate }"
-            :to="
-              $localePath({
-                name: 'collections-slug',
-                params: {
-                  slug: collection?.slug,
-                },
-              })
-            "
+      <NavigationMenuItem v-for="collection in localeCollections">
+        <NuxtLink
+          custom
+          v-slot="{ isActive, href, navigate }"
+          :to="
+            $localePath({
+              name: 'collections-slug',
+              params: {
+                slug: collection?.slug,
+              },
+            })
+          "
+        >
+          <NavigationMenuLink
+            :active="isActive"
+            :href
+            :class="navigationMenuTriggerStyle()"
+            @click="navigate"
           >
-            <NavigationMenuLink
-              :active="isActive"
-              :href
-              :class="navigationMenuTriggerStyle()"
-              @click="navigate"
-            >
-              {{ collection?.name }}
-            </NavigationMenuLink>
-          </NuxtLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  </main>
+            {{ collection?.name }}
+          </NavigationMenuLink>
+        </NuxtLink>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
 </template>
