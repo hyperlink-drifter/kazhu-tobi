119c98b92a72ff0902775f23a5e5e6ce02fd5a32
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-04 18:13:47 +0200

    refactor: use drawer for mobile navigation

--- app/components/TheNavigation.vue
+++ app/components/TheNavigation.vue
@@ -2,7 +2,7 @@
 import type { HTMLAttributes } from 'vue';
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
-import { Menu, CircleX } from 'lucide-vue-next';
+import { CircleX, AlignJustify } from 'lucide-vue-next';
 
 interface Props {
   class?: HTMLAttributes['class'];
@@ -24,61 +24,75 @@ const localeCollections = computed(() => {
   );
 });
 
-const isSheetOpen = ref(false);
+const isDrawerOpen = ref(false);
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const isLgAndLarger = breakpoints.greaterOrEqual('lg');
 
 watch(isLgAndLarger, (isLgAndLarger) => {
   if (isLgAndLarger) {
-    isSheetOpen.value = false;
+    isDrawerOpen.value = false;
   }
 });
 </script>
 
 <template>
-  <Sheet v-model:open="isSheetOpen">
-    <SheetTrigger class="lg:hidden">
-      <Menu />
-    </SheetTrigger>
-    <SheetContent side="left" class="inset-2 w-auto h-auto lg:hidden">
-      <SheetClose
-        class="w-fit p-2 ring-offset-background data-[state=open]:bg-secondary rounded-xs opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none"
-      >
-        <CircleX class="size-8" />
-        <span class="sr-only">Close</span>
-      </SheetClose>
-      <NavigationMenu class="flex-0">
-        <NavigationMenuList class="flex flex-col items-start">
-          <NavigationMenuItem
-            v-for="collection in localeCollections"
-            :key="`burger-menu-${collection?.slug}`"
-          >
-            <NuxtLink
-              custom
-              v-slot="{ isActive, href, navigate }"
-              :to="
-                $localePath({
-                  name: 'collections-slug',
-                  params: {
-                    slug: collection?.slug,
-                  },
-                })
-              "
+  <Drawer v-model:open="isDrawerOpen">
+    <DrawerTrigger class="lg:hidden" as-child>
+      <Button variant="ghost" size="icon">
+        <AlignJustify />
+      </Button>
+    </DrawerTrigger>
+    <DrawerContent class="">
+      <div class="p-4">
+        <NavigationMenu
+          orientation="vertical"
+          class="max-w-full justify-start md:justify-center"
+        >
+          <NavigationMenuList class="block">
+            <NavigationMenuItem
+              v-for="collection in localeCollections"
+              :key="`menu-${collection?.slug}`"
             >
-              <NavigationMenuLink
-                :active="isActive"
-                :href
-                :class="navigationMenuTriggerStyle()"
-                @click="navigate"
+              <NuxtLink
+                custom
+                v-slot="{ isActive, href, navigate }"
+                :to="
+                  $localePath({
+                    name: 'collections-slug',
+                    params: {
+                      slug: collection?.slug,
+                    },
+                  })
+                "
               >
-                {{ collection?.name }}
-              </NavigationMenuLink>
-            </NuxtLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </SheetContent>
-  </Sheet>
+                <NavigationMenuLink
+                  :href
+                  :active="isActive"
+                  :class="`p-0! ${navigationMenuTriggerStyle()}`"
+                  @click="
+                    () => {
+                      isDrawerOpen = false;
+                      navigate();
+                    }
+                  "
+                >
+                  {{ collection?.name }}
+                </NavigationMenuLink>
+              </NuxtLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <DrawerFooter class="items-center">
+        <DrawerClose
+          class="grid grid-cols-[1fr_auto_1fr] w-full md:w-md"
+          as-child
+        >
+          <Button variant="default"> <CircleX /> {{ $t('close') }} </Button>
+        </DrawerClose>
+      </DrawerFooter>
+    </DrawerContent>
+  </Drawer>
   <NavigationMenu class="hidden lg:flex">
     <NavigationMenuList>
       <NavigationMenuItem
