02446a429ae41b4698fee6afe01e0b97f9cd1cea
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-04 20:39:28 +0200

    feat: initial setup for multiple menu options

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { HTMLAttributes } from 'vue';
+import { ShoppingCart, Heart, Search, User } from 'lucide-vue-next';
 
 interface Props {
   class?: HTMLAttributes['class'];
@@ -9,11 +10,25 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <LayoutCenter>
+  <LayoutCenter class="sticky top-0 bg-white z-20 py-2 lg:py-0">
     <header :class="cn('flex justify-between', props.class)">
       <slot>
         <TheNavigation />
-        <TheLocaleSelector />
+        <div class="flex items-center gap-2 py-4">
+          <TheLocaleSelector />
+          <Button>
+            <Search />
+          </Button>
+          <Button>
+            <User />
+          </Button>
+          <Button>
+            <Heart />
+          </Button>
+          <Button>
+            <ShoppingCart />
+          </Button>
+        </div>
       </slot>
     </header>
   </LayoutCenter>
