6f05e5660bbb4db8b6bf12b27996ef74065effde
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-06 01:44:56 +0200

    feat: implement layout component cover

---
+++ app/components/layout/Cover.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue';
+
+interface Props {
+  class?: HTMLAttributes['class'];
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'flex flex-col min-h-svh p-4 [&>*:first-child:not(.center)]:mt-0 [&>*:last-child:not(.center)]:mb-0 [&_.center]:my-auto ',
+        props.class
+      )
+    "
+  >
+    <div>
+      <slot name="top" />
+    </div>
+    <div class="center">
+      <slot name="center" />
+    </div>
+    <div>
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
