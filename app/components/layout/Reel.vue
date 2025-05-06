8f010807ef5e137bf6c49a6e86ed632a5f616922
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-06 17:19:17 +0200

    feat: implement layout component reel

---
+++ app/components/layout/Reel.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { HTMLAttributes } from 'vue';
+
+interface Props {
+  class?: HTMLAttributes['class'];
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div :class="cn('reel', props.class)">
+    <slot />
+  </div>
+</template>
