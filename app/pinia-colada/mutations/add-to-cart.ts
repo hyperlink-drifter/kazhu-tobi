b6f50859bbe536749b8f0c88cddac5fe6cc62879
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-06-11 23:14:09 +0200

    feat: open cart after item was added

--- app/pinia-colada/mutations/add-to-cart.ts
+++ app/pinia-colada/mutations/add-to-cart.ts
@@ -3,6 +3,7 @@ import type { AddItemToOrderMutation } from '@@/graphql/generated';
 export const useAddItemToCart = defineMutation(() => {
   const productVariantId = ref('');
   const queryCache = useQueryCache();
+  const isCartOpen = useState('is-cart-open', () => false);
 
   const { mutate, ...mutation } = useMutation({
     mutation: (productVariantId: string) =>
@@ -16,6 +17,8 @@ export const useAddItemToCart = defineMutation(() => {
     async onSettled() {
       // Invalidate the query to refetch the new cart
       await queryCache.invalidateQueries({ key: ['cart'] });
+
+      isCartOpen.value = true;
     },
   });
 
